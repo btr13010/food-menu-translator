2a5189408308e0578d90bc0ac6c2c5e2de3044b5
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2023-02-04 18:01:43 +0700

    finalise

--- front-end/src/components/Main/Main.jsx
+++ front-end/src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import './Main.css';
 import Table from '../Table/Table';
 
 const Main = (props) => {
+  console.log(props.restaurantData)
   return (
     <div className='Main'>
       <Table data={props.restaurantData} changeData={(e)=>props.changeData(e)}/>
